7c3b8676dc1a55a5364f5ada7156fae44056da38
lawalalx <lawal.alx@gmail.com>
2024-02-17 11:28:16 +0100

    test

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a speech to text and diarization project',
+    author='Olanrewaju Lawal',
+    license='MIT',
+)
